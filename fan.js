4cc9aba964c387aaec6991848767cf271fdad632
AWESOME319 <83673925+AWESOME319@users.noreply.github.com>
2022-12-25 11:46:56 -0500

    Add files via upload

---
+++ fan.js
@@ -0,0 +1,30 @@
+fanimg=""
+Status=""
+function preload(){
+fanimg=loadImage('fan.jpg');
+}
+function setup(){
+    canvas=createCanvas(640,420);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status:Detecting Objects";
+}
+function draw()
+{
+    image(fanimg,0,0,640,420);
+}
+function modelLoaded(){
+    console.log("Model Loaded ")
+    Status=true;
+    objectDetector.detect(img,gotResult);
+}
+    function gotResult(error,results){
+        if(error){
+            console.log(error);
+        }
+        console.log(results);
+      }
+back()
+{
+    window.location("index.html");
+}
---
+++ fruit.js
@@ -0,0 +1,30 @@
+fruitimg=""
+Status=""
+function preload(){
+    fruitimg=loadImage('fruit.jpg');
+}
+function setup(){
+    canvas=createCanvas(640,420);
+    canvas.center();
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status:Detecting Objects";
+}
+function draw()
+{
+    image(closetimg,0,0,640,420);
+}
+function modelLoaded(){
+    console.log("Model Loaded ")
+    Status=true;
+    objectDetector.detect(img,gotResult);
+}
+    function gotResult(error,results){
+        if(error){
+            console.log(error);
+        }
+        console.log(results);
+      }
+back()
+{
+    window.location("index.html");
+}
